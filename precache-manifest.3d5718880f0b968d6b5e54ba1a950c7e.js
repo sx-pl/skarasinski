30661ee4e91a8b4fddf26d9f3a7824afbdd16cf7
Sebastian <soxiuy@gmail.com>
2021-04-26 11:50:55 +0200

    Updates

---
+++ precache-manifest.3d5718880f0b968d6b5e54ba1a950c7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb870140388d12523249",
+    "url": "/skarasinski/static/css/main.cf3d743f.chunk.css"
+  },
+  {
+    "revision": "cb870140388d12523249",
+    "url": "/skarasinski/static/js/main.b86b04e2.chunk.js"
+  },
+  {
+    "revision": "8d015490771fb654e967",
+    "url": "/skarasinski/static/js/runtime~main.69260071.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/skarasinski/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "839c08ab1027c65d8288c5a4b45174d8",
+    "url": "/skarasinski/index.html"
+  }
+];
